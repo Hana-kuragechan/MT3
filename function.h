6a5ff880b0b68cd8afeabf2d810a99d13acd2155
Hana-kuragechan <k023g1049@g.neec.ac.jp>
2024-06-04 12:49:43 +0900

    00_03確認課題完了

--- function.h
+++ function.h
@@ -19,6 +19,7 @@ Vector3 Multiply(float scalar, const Vector3& v);
 float Dot(const Vector3& v1, const Vector3& v2);
 float Length(const Vector3& v);
 Vector3 Normalize(const Vector3& v);
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
 static const int kColumnWidth = 60;
 void VectorScreenprintf(int x, int y, const Vector3& vector, const char* label);
 
--- main.cpp
+++ main.cpp
@@ -9,10 +9,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
-	Vector3 v1{ 1.0f,3.0f,-5.0f };
-	Vector3 v2{ 4.0f,-1.0f,2.0f };
-	float k = { 4.0f };
-	const int kRowHeight = 30;
+	Vector3 translate{ 4.1f,2.6f,0.8f };
+	Vector3 scale{ 1.5f,5.2f,7.3f };
+	Vector3 point{ 2.3f,3.8f,1.4f };
+	Matrix4x4 transformMatrix = {
+		1.0f,2.0f,3.0f,4.0f,
+		3.0f,1.0f,1.0f,2.0f,
+		1.0f,4.0f,2.0f,3.0f,
+		2.0f,2.0f,1.0f,3.0f,
+	};
+	
+
+	
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
@@ -30,15 +38,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		
+		Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+		Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+		Vector3 transformed = Transform(point, transformMatrix);
 		///
 		/// ↑更新処理ここまで
 		///
-		
+
 		///
 		/// ↓描画処理ここから
 		///
-
+		VectorScreenprintf(0, 0, transformed, "transformed");
+		MatrixScreenPrintf(0, kRowHeight*2, translateMatrix);
+		MatrixScreenPrintf(0, kRowHeight*7, scaleMatrix);
 		///
 		/// ↑描画処理ここまで
 		///
